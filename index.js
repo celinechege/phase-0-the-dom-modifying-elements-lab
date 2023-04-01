bf32854764cb5fa61e240c8adc3240a9b7a573e7
celinechege <chegeceline18@gmail.com>
2023-04-01 15:08:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// find and remove the main#main node
+const main = document.querySelector('main#main');
+main.remove();
+
+// create a new header element with the text "YOUR-NAME is the champion"
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// add the new header element to the document
+const body = document.querySelector('body');
+body.appendChild(newHeader);
